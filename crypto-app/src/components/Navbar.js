344520d693c31945f367542f855561445d43623e
aniketbisht24 <aniketbisht24@gmail.com>
2022-04-05 08:58:15 +0530

    crypto

--- crypto-app/src/components/Navbar.js
+++ crypto-app/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Button, Menu, Typography, Avatar } from 'antd'
 import {Link} from 'react-router-dom'
-import {HomeOutlined, MoneyCollectOutlined, BulbOutline, FundOutlined, MenuOutlined} from '@ant-design/icons'
+import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from '@ant-design/icons'
 import icon from '../images/cryptocurrency.png'
 
 
@@ -19,6 +19,23 @@ export const Navbar = () => {
 
                 </Button> */}
         </div>
+        <Menu theme="dark">
+          <Menu.Item icon = {<HomeOutlined />}>
+            <Link to ="/"> Home</Link>
+          </Menu.Item>
+          <Menu.Item icon = {<FundOutlined />}>
+            <Link to ="/cryptocurrencies"> Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon = {<MoneyCollectOutlined />}>
+            <Link to ="/exchanges"> Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon = {<BulbOutlined />}>
+            <Link to ="/news"> News</Link>
+          </Menu.Item>
+          <Menu.Item icon = {<HomeOutlined />}>
+            <Link to ="/"> Home</Link>
+          </Menu.Item>
+        </Menu>
     </div>
   )
 }
--- crypto-app/src/index.js
+++ crypto-app/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
 );

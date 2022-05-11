a34cee767606bc5f6344963762c82c95ba468e07
aniketbisht24 <aniketbisht24@gmail.com>
2022-05-11 22:50:21 +0530

    frontend start

--- crypto-app-frontend/src/App.js
+++ crypto-app-frontend/src/App.js
@@ -1,25 +1,13 @@
-import React from 'react'
-import {Switch, Route, Link} from 'react-router-dom'
-import {Layout, Typography, Space} from 'antd'
-import {Navbar} from './components'
 import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <div className="app">
-      
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="main">
-
-      </div>
-      <div className="footer">
-
-      </div>
+    <div className="App">
+        <p>
+          hello hi
+        </p>
     </div>
-
-    )
+  );
 }
 
-export default App
+export default App;
